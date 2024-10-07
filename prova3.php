234b21006175248c86b0a23b1b5925d00a06d0fb
tgrynkiv <tgrynkiv@inscaparrella.cat>
2024-10-07 19:34:56 +0200

    php

---
+++ prova3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Page Title</title>
+</head> 
+<body> 
+    
+<?php
+$txt = "PHP";
+
+echo "<h2>$txt</h2>";
+?>
+
+</body> 
+</html> 
